ee61757c5d0c4d9cc6e67b77fef766d0eeaaed63
jypkr <wodusqkr1210@gmail.com>
2021-10-12 23:58:34 -0700

    completed

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // array of questions for user
 const questions = [
-
+	{
+		type: 'input',
+		name: 'title',
+		message: 'Title?'
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'description?'
+	},
+	{
+		type: 'input',
+		name: 'installation',
+		message: 'Installation process?'
+	},
+	{
+		type: 'input',
+		name: 'usage',
+		message: 'usage?'
+	},
+	{
+		type: 'input',
+		name: 'licenses',
+		message: 'licenses?'
+	},
+	{
+		type: 'input',
+		name: 'contribute',
+		message: 'contribute?'
+	},
+	{
+		type: 'input',
+		name: 'tests',
+		message: 'tests?'
+	}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, err => {
+		if (err) {
+			throw err;
+		}
+		console.log('Created!')
+	})
 }
 
 // function to initialize program
-function init() {
-
+async function init() {
+	const answers = await inquirer.prompt(questions)
+	writeToFile('./output/README.md', generateMarkdown(answers))
 }
 
 // function call to initialize program
---
+++ output/README.md
@@ -0,0 +1,28 @@
+
+
+# test ![GitHub package.json version](https://img.shields.io/github/package-json/v/WarriorofZarona/Good-README-Generator)
+testing
+# Tables of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Credits](#credits)
+# Installation
+haha
+# Usage
+undefined
+# License
+idk
+# Contributing
+3
+# Tests
+hahahaha
+# Questions
+![Profile Avatar](undefined)
+If you have any questions, please e-mail me at undefined.
+# Credits
+Github: [undefined](undefined)
+Copyright undefined. All Rights Reserved.
